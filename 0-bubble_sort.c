2d0177aa3ff9c277e4252308613bfae1eb2853f1
Farahrama <rafarah@gmail.com>
2024-12-08 17:33:48 -0800

    j

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,15 +7,19 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    for (int i = 0; i < size - 1; i++)
-    {
-        for (int j = 0; j < size - i - 1; j++)
+    size_t i, j;
+    int temp;
+    if(array == NULL || size < 2)
+	    return;
+    for (i = 0; i < size - 1; i++){
+        for (j = 0; j < size - i - 1; j++)
         {
             if (array[j] > array[j + 1])
             {
-                int temp = array[j];
+                temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
+                print_array(array, size);
             }
         }
     }
