fcdd29086e904e31ee116fbe1e08802a2ea48da3
Farahrama <rafarah@gmail.com>
2024-12-08 17:54:32 -0800

    k

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,23 +4,25 @@
  *
  * @array: array of elements
  *
+ * @size: size of array
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int temp;
-    if(array == NULL || size < 2)
-	    return;
-    for (i = 0; i < size - 1; i++){
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                print_array(array, size);
-            }
-        }
-    }
+size_t i, j;
+int temp;
+if(array == NULL || size < 2)
+return;
+for (i = 0; i < size - 1; i++)
+{
+for (j = 0; j < size - i - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
+print_array(array, size);
+}
+}
+}
 }
